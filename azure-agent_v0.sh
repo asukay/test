1f1ecf74df00faa900cdd350fb3a9c9af13c99f9
asukay <manh.dam@live.fr>
2021-05-10 15:28:43 +0200

    az do agent sh

--- azure-agent_v0.sh
+++ azure-agent_v0.sh
@@ -26,15 +26,31 @@ if [ -z "${config_agentname}" ];then
    exit 1
 fi
 
+echo $agent_folder 
+echo $config_serverurl
+echo $config_pat_token
+echo $config_agentname
+
 cd $agent_folder
 wget https://vstsagentpackage.azureedge.net/agent/2.185.1/vsts-agent-linux-arm64-2.185.1.tar.gz
-tar zxvf vsts-agent-linux-arm64-2.185.1.tar.gz
+tar -zxvf vsts-agent-linux-arm64-2.185.1.tar.gz
+
+echo $agent_folder 
+
+echo $config_serverurl
+echo $config_pat_token
+echo $config_agentname
+
+echo "chmod +x $agent_folder -R"
 
 chmod +x $agent_folder -R
 
+echo "./bin/installdependencies.sh"
 ./bin/installdependencies.sh
 
 apt upgrade -y
+
+echo "./config.sh"
 
 ./config.sh --unattended --url $config_serverurl --auth pat --token $config_pat_token --pool "TeamBreak-DonutQuizz" --agent "$config_agentname" --acceptTeeEula 
 
