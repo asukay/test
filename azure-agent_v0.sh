5da54052daec6be6d0d887cca44c286d710229fd
asukay <manh.dam@live.fr>
2021-05-10 15:33:07 +0200

    sss

--- azure-agent_v0.sh
+++ azure-agent_v0.sh
@@ -2,10 +2,10 @@
 
 agent_folder="/data/dq/ms-agent"
 if ! [ -d $agent_folder ];then
-   mkdir $agent_folder -p
+   sudo mkdir $agent_folder -p
 fi
 
-chown ubuntu:ubuntu $agent_folder
+sudo chown ubuntu:ubuntu $agent_folder
 
 config_serverurl="$1"
 config_pat_token="$2"
@@ -43,19 +43,19 @@ echo $config_agentname
 
 echo "chmod +x $agent_folder -R"
 
-chmod +x $agent_folder -R
+sudo chmod +x $agent_folder -R
 
 echo "./bin/installdependencies.sh"
-./bin/installdependencies.sh
+sudo ./bin/installdependencies.sh
 
-apt upgrade -y
+sudo apt upgrade -y
 
 echo "./config.sh"
 
 ./config.sh --unattended --url $config_serverurl --auth pat --token $config_pat_token --pool "TeamBreak-DonutQuizz" --agent "$config_agentname" --acceptTeeEula 
 
-chmod +x $agent_folder -R
+sudo chmod +x $agent_folder -R
 
-./svc.sh install 
+sudo ./svc.sh install 
 
 
